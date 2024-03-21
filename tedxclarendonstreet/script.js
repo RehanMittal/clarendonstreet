64e77a45e684009d48234fc03b55c37d2e012913
Rehan Mittal <115777267+RehanMittal@users.noreply.github.com>
2024-03-21 17:05:44 +0530

    Add files via upload

---
+++ tedxclarendonstreet/script.js
@@ -0,0 +1,36 @@
+
+function loaderAnimation() {
+    var loader = document.querySelector("#loader")
+    setTimeout(function () {
+        loader.style.top = "-100%"
+    }, 4200)
+}
+
+function swiperAnimation() {
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: "auto",
+        centeredSlides: true,
+        spaceBetween: 100,
+    });
+}
+function page4Animation() {
+    var elemC = document.querySelector("#elem-container")
+    var fixed = document.querySelector("#fixed-image")
+    elemC.addEventListener("mouseenter", function () {
+        fixed.style.display = "block"
+    })
+    elemC.addEventListener("mouseleave", function () {
+        fixed.style.display = "none"
+    })
+
+    var elems = document.querySelectorAll(".elem")
+    elems.forEach(function (e) {
+        e.addEventListener("mouseenter", function () {
+            var image = e.getAttribute("data-image")
+            fixed.style.backgroundImage = `url(${image})`
+        })
+    })
+}
+swiperAnimation()
+loaderAnimation()
+page4Animation() 
